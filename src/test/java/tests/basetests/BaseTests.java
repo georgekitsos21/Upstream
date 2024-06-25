736605dd4adf167497d3a3979b17ea4e971ba208
georgekitsos21 <georgekitsos20@gmail.com>
2024-06-25 08:56:20 +0300

    add tearDown in the basetests

--- src/test/java/tests/basetests/BaseTests.java
+++ src/test/java/tests/basetests/BaseTests.java
@@ -2,6 +2,7 @@ package tests.basetests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import pages.BooksPage;
 import pages.HomePage;
@@ -25,5 +26,9 @@ public class BaseTests {
         loginPage = new LoginPage(driver);
         profilePage = new ProfilePage(driver);
         booksPage = new BooksPage(driver);
+    }
+    @AfterClass
+    public void tearDown(){
+        driver.close();
     }
 }
